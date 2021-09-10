516a6f9ac8e0d5f34cba00e0ec22c54ea2fad710
JCuero-BE <jcuero-oic-be@contractorcampus.com>
2021-09-10 15:36:21 -0500

    ejercicio 1

---
+++ 7.ejercicios/ejercicio1.dart
@@ -0,0 +1,17 @@
+import 'dart:ffi';
+import 'dart:math';
+
+main() {
+  var random = new Random();
+  List<dynamic> ages = new List.generate(
+      6, (_) => random.nextInt(90) + 10); //edades entre 10 y 99
+
+  var average = (ages.reduce((a, b) => a + b) / ages.length).toStringAsFixed(2);
+  var menor = ages.cast<num>().reduce(min);
+  var mayor = ages.cast<num>().reduce(max);
+
+  print("Edades: ${ages}");
+  print("Edad menor: ${menor}");
+  print("Edad mayor: ${mayor}");
+  print("Promedio edades: ${average}");
+}
